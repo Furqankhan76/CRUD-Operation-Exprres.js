bc1a01f0ce993e7187fa90201437ce6c4aab8479
furqan <furqankhanfk0999@gmail.com>
2024-11-09 16:58:47 +0530

    winston and morgan added

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
+// import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let data = [];
 let nextid = 1;
@@ -11,6 +31,7 @@ let nextid = 1;
 //add a new tea
 
 app.post("/teas", (req, res) => {
+  logger.warn("A post request is made to add a new tea ");
   req.body.price;
   const { name, price } = req.body;
   const newtea = { id: nextid++, name, price };
@@ -46,6 +67,8 @@ app.put("teas", (req, res) => {
   tea.price = price;
   res.send(200).send(tea);
 });
+
+//delete teas with id'sa
 
 app.delete("/teas/:id", (req, res) => {
   data.findIndex((t) => t.id === parseInt(req.params.id));
